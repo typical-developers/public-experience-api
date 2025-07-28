803bf8b32f0594cb358cdd77991589ffcc4ab025
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-07-28 13:10:05 -0400

    fix: remove set env for opencloud

--- internal/experiences/opencloud.go
+++ internal/experiences/opencloud.go
@@ -1,11 +1,9 @@
 package experiences
 
 import (
-	"os"
-
 	"github.com/typical-developers/goblox/opencloud"
 )
 
 var (
-	Opencloud = opencloud.NewClient().WithAPIKey(os.Getenv("OPENCLOUD_API_KEY"))
+	Opencloud = opencloud.NewClient()
 )
