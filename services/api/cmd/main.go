5e07bac818c978f1ce3550d38a65c90d693e167a
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-07-28 13:10:26 -0400

    fix: set api key

--- services/api/cmd/main.go
+++ services/api/cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/typical-developers/public-experience-api/internal/docs"
+	"github.com/typical-developers/public-experience-api/internal/experiences"
 	"github.com/typical-developers/public-experience-api/services/api/config"
 	"github.com/typical-developers/public-experience-api/services/api/endpoints"
 )
 
 func main() {
 	config.Load()
+	experiences.Opencloud.WithAPIKey(config.OpencloudAPIKey)
 
 	app := fiber.New()
 	endpoints.Register(app)
