c48a528d93930e36eb1dc842bbc5d60d2d12a3c1
LuckFire <39076891+LuckFire@users.noreply.github.com>
2026-03-07 01:49:57 -0500

    chore: initalize openapi docs

--- cmd/public/main.go
+++ cmd/public/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"github.com/typical-developers/public-experience-api/cmd/public/config"
+	_ "github.com/typical-developers/public-experience-api/cmd/public/docs"
+)
+
+// @title           Typical Developers - Public Experience API
+// @version         1.0
+// @description     This is a publicly accessible API to get data in Typical Developers experiences.
+//
+// @host      localhost:8080
+// @BasePath  /v1/
+func main() {
+	r := chi.NewMux()
+	r.Get("/docs/*", httpSwagger.Handler())
+
+	port := fmt.Sprintf(":%s", config.C.Port)
+	panic(http.ListenAndServe(port, r))
 }
