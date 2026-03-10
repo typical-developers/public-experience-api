eb8e16acc2406d644b4f5c4fc5e948423e239ca5
LuckFire <39076891+LuckFire@users.noreply.github.com>
2026-03-10 19:26:35 -0400

    refactor(public): remove rate-limit on server side any implementation of rate-limits are going to be done through cloudflare due to caching

--- cmd/public/handlers/oaklands_v1/handler.go
+++ cmd/public/handlers/oaklands_v1/handler.go
@@ -2,14 +2,10 @@ package oaklands_v1
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/httprate"
 	"github.com/typical-developers/goblox/opencloud"
-	models "github.com/typical-developers/public-experience-api/cmd/public/handlers"
 	"github.com/typical-developers/public-experience-api/internal/oaklands"
-	"github.com/typical-developers/public-experience-api/pkg/httpx"
 )
 
 type OaklandsV1Opts struct {
@@ -20,22 +16,6 @@ type OaklandsV1Opts struct {
 type OaklandsV1Routes struct {
 	opencloud *opencloud.Client
 	uc        oaklands.OaklandsUsecase
-}
-
-func oaklandsRateLimit(limit int, window time.Duration, bucket string) func(http.Handler) http.Handler {
-	return httprate.Limit(limit, window,
-		httprate.WithKeyFuncs(
-			httprate.Key("oaklands:"+bucket),
-			httprate.KeyByIP,
-			httprate.KeyByEndpoint,
-		),
-		httprate.WithLimitHandler(func(w http.ResponseWriter, r *http.Request) {
-			_ = httpx.WriteJSON(w, models.ErrorResponse{
-				Type:    "HttpErrorTooManyRequest",
-				Message: "You are being rate limited.",
-			}, http.StatusTooManyRequests)
-		}),
-	)
 }
 
 func cacheControl() func(http.Handler) http.Handler {
@@ -55,8 +35,6 @@ func NewOaklandsV1(r *chi.Mux, opts *OaklandsV1Opts) {
 	}
 
 	r.Route("/v1/oaklands", func(r chi.Router) {
-		r.Use(oaklandsRateLimit(120, 1*time.Minute, "per-minute"))
-		r.Use(oaklandsRateLimit(6, 1*time.Second, "per-second"))
 		r.Use(cacheControl())
 
 		r.Route("/economy", func(r chi.Router) {
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/caarlos0/env/v10 v10.0.0
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.2
-	github.com/go-chi/httprate v0.15.0
 	github.com/joho/godotenv v1.5.1
 	github.com/redis/go-redis/v9 v9.18.0
 	github.com/robfig/cron/v3 v3.0.1
@@ -27,10 +26,8 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
-	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/mod v0.17.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
