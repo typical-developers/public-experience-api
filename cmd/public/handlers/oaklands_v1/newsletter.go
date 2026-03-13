8810839d7577e4b210f451fe825b0a94736a4937
LuckFire <39076891+LuckFire@users.noreply.github.com>
2026-03-13 16:17:15 -0400

    chore(docs): update newsletter descriptor chore(docs): add basic metadata

--- cmd/public/handlers/oaklands_v1/newsletter.go
+++ cmd/public/handlers/oaklands_v1/newsletter.go
@@ -88,7 +88,7 @@ func (o *OaklandsV1Routes) GetNewsletters(w http.ResponseWriter, r *http.Request
 //	@Tags			Oaklands
 //
 //	@Param			If-None-Match	header		string	false	"ETag to validate cached response."
-//	@Param			id				path		string	true	"The ID of the newsletter."	default(latest)
+//	@Param			id				path		string	true	"The id of the newsletter. For quick access to the latest newsletter, use 'latest' as the value."	default(latest)
 //
 //	@Success		200				{object}	object{data=Newsletter}
 //	@Failure		503				{object}	models.ErrorResponse
