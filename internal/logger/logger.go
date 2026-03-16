81cf284a1c1e084be9d12820523e4c3f349d4f1f
LuckFire <39076891+LuckFire@users.noreply.github.com>
2026-03-16 17:24:25 -0400

    refactor: logger config

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,23 +2,61 @@ package logger
 
 import (
 	"os"
+	"strings"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func init() {
+// createLogger will look for ENVIRONMENT.
+// If the environment is DEVELOPMENT, it will use NewDevelopment.
+// Otherwise, will use NewProduction.
+func createLogger() *zap.Logger {
 	var z *zap.Logger
-	var err error
 
-	if v := os.Getenv("ENVIRONMENT"); v == "DEVELOPMENT" {
-		z, err = zap.NewDevelopment()
+	environment := strings.ToLower(os.Getenv("ENVIRONMENT"))
+
+	if environment == "development" {
+		z = zap.Must(zap.NewDevelopment())
 	} else {
-		z, err = zap.NewProduction()
+		z = zap.Must(zap.NewProduction())
 	}
 
-	if err != nil {
-		panic(err)
+	return z
+}
+
+// getLogLevel will look for LOG_LEVEL in the environment.
+// defaults to Info.
+func getLogLevel() zapcore.Level {
+	level := strings.ToLower(os.Getenv("LOG_LEVEL"))
+
+	switch level {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "warn":
+		return zap.WarnLevel
+	case "error":
+		return zap.ErrorLevel
+	case "dpanic":
+		return zap.DPanicLevel
+	case "panic":
+		return zap.PanicLevel
+	case "fatal":
+		return zap.FatalLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func init() {
+	logger := createLogger()
+
+	config := zap.Config{
+		Level: zap.NewAtomicLevelAt(getLogLevel()),
 	}
 
-	zap.ReplaceGlobals(z)
+	zap.Must(config.Build())
+	zap.ReplaceGlobals(logger)
 }
