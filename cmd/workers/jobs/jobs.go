beeba76b68d0f1a13b7d0660a7f43976afa1ed12
LuckFire <39076891+LuckFire@users.noreply.github.com>
2026-03-16 23:26:54 -0400

    chore(workers): set retry attempts to 1 if 0

--- cmd/workers/jobs/jobs.go
+++ cmd/workers/jobs/jobs.go
@@ -19,6 +19,10 @@ type RetryConfig struct {
 // TODO: Add some sort of InFlight check to prevent jobs from re-running while they are actively being retried.
 // For the time being, this implementation is just to make sure the jobs will actually retry on failure.
 func WithRetry(c RetryConfig) cron.JobWrapper {
+	if c.RetryAttempts <= 0 {
+		c.RetryAttempts = 1
+	}
+
 	job := func(j cron.Job) {
 		for attempt := 1; attempt <= c.RetryAttempts; attempt++ {
 			failed := false
